c96d0411cd5977fc1659fb2c2d7cb549c01f4a56
Gary Broderick <garybroderick82@gmail.com>
2024-10-15 15:37:10 +0000

    Debug test1

--- failte_bistro/settings.py
+++ failte_bistro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_2qd-q#9vdnvvby&qr*e*hkxa@=y@et5sk%fof^vs4j*t^3)@3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-gbroder24-djangothefail-q5n6etlyjeb.ws.codeinstitute-ide.net', 
                 '.herokuapp.com',]
