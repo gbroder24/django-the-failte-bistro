918acda4325c988cf1370001af678287e8ead885
Gary Broderick <garybroderick82@gmail.com>
2024-10-15 15:23:01 +0000

    Initial setup and commit of failte bistro project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn failte_bistro.wsgi
---
+++ failte_bistro/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for failte_bistro project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'failte_bistro.settings')
+
+application = get_asgi_application()
