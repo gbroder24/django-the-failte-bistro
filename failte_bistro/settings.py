eec928b801b4068a4a1589104859a72d402e6812
Gary Broderick <garybroderick82@gmail.com>
2024-10-17 15:14:36 +0000

    Create a secret key

--- failte_bistro/settings.py
+++ failte_bistro/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_2qd-q#9vdnvvby&qr*e*hkxa@=y@et5sk%fof^vs4j*t^3)@3'
+# SECRET_KEY = 'django-insecure-_2qd-q#9vdnvvby&qr*e*hkxa@=y@et5sk%fof^vs4j*t^3)@3'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
