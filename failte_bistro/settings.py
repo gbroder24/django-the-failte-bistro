bef52fe5ed8dbd51db1221e12eaf297d95ce8aa3
Gary Broderick <garybroderick82@gmail.com>
2024-11-29 17:34:19 +0000

    Update failte settings and urls apps for validation errors

--- failte_bistro/settings.py
+++ failte_bistro/settings.py
@@ -32,8 +32,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-gbroder24-djangothefail-p4fqfv722ar.ws.codeinstitute-ide.net', 
-                '.herokuapp.com',]
+ALLOWED_HOSTS = [
+    '8000-gbroder24-djangothefail-p4fqfv722ar.ws.codeinstitute-ide.net',
+    '.herokuapp.com', ]
 
 
 # Application definition
@@ -115,17 +116,18 @@ CSRF_TRUSTED_ORIGINS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+ {
+    'NAME':
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+ {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+ {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+ {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
